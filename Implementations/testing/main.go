3f95cdd7afc414b09464d36cc74216346d00d7d4
Girgis Emad <girgisemad74@gmail.com>
2024-09-19 07:24:44 +0300

    Work on ch3

---
+++ Implementations/testing/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+
+func main(){
+var i int = 3
+for i<4 {
+ fmt.Println(i)
+ i++
+}
+
+}
--- Theory/Ch3.go
+++ Theory/Ch3.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func main() {
+func main123() {
 	var p = new(int)
        fmt.Println(p)
 }
